37efe41635b8d19b9eed74c41ff29657f9029201
unknown <dhikramadhan008@gmail.com>
2023-12-13 08:25:51 +0700

    update ikon

--- app/src/main/java/com/example/donorin/listRiwayat.java
+++ app/src/main/java/com/example/donorin/listRiwayat.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -17,16 +19,28 @@ import retrofit2.Response;
 public class listRiwayat extends AppCompatActivity {
 
     RecyclerView rvRiwayat;
+    TextView btnRiwayat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riwayat);
         rvRiwayat = findViewById(R.id.rvRiwayat);
+        btnRiwayat = findViewById(R.id.btnRiwayat);
+
         Intent intent = getIntent();
         String nik = intent.getExtras().getString("nik");
 
         getRiwayat(nik);
+
+        btnRiwayat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPindah = new Intent(listRiwayat.this, listRspmi.class);
+
+                startActivity(intentPindah);
+            }
+        });
     }
 
     private void getRiwayat(String nik) {
